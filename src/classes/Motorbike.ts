81b8526e5dcad57abae71e1f10c5a7eb0c19494d
Falmanza94 <fischer.almanza@gmail.com>
2025-02-16 17:36:46 -0600

    edited console log to show wheel 1/2

--- src/classes/Motorbike.ts
+++ src/classes/Motorbike.ts
@@ -61,7 +61,13 @@ wheels: Wheel[];
   console.log(`Year: ${this.year}`);
   console.log(`Weight: ${this.weight}`);
   console.log(`Top Speed: ${this.topSpeed}`);
-  console.log(`Wheel: ${this.wheels}`);
+  console.log(`Color ${this.color}`);
+  console.log(
+    `Wheel 1: ${this.wheels[0].getDiameter} inch with a ${this.wheels[0].getTireBrand} tire`
+  );
+  console.log(
+    `Wheel 2: ${this.wheels[1].getDiameter} inch with a ${this.wheels[1].getTireBrand} tire`
+  );
   }
 }
 // Export the Motorbike class as the default export
--- src/classes/Truck.ts
+++ src/classes/Truck.ts
@@ -6,14 +6,42 @@ import Wheel from './Wheel.js';
 import AbleToTow from '../interfaces/AbleToTow.js';
 
 // TODO: The Truck class should extend the Vehicle class and should implement the AbleToTow interface
-class Truck {
+class Truck extends Vehicle implements AbleToTow {
   // TODO: Declare properties of the Truck class
   // TODO: The properties should include vin, color, make, model, year, weight, top speed, wheels, and towing capacity
   // TODO: The types should be as follows: vin (string), color (string), make (string), model (string), year (number), weight (number), topSpeed (number), wheels (Wheel[]), towingCapacity (number)
-
+vin: string;
+color: string;
+make: string;
+model: string;
+year: number;
+weight: number;
+topSpeed: number;
+wheels: Wheel[];
+towingCapacity: number;
   // TODO: Create a constructor that accepts the properties of the Truck class
+  constructor(
+    vin: string,
+    color: string,
+    make: string,
+    model: string,
+    year: number,
+    weight: number,
+    topSpeed: number,
+    wheels: Wheel[],
+    towingCapacity: number
+  ){
     // TODO: The constructor should call the constructor of the parent class, Vehicle
+    super();
     // TODO: The constructor should initialize the properties of the Truck class
+    this.vin = vin;
+    this.color = color;
+    this.make = make;
+    this.model = model;
+    this.year = year;
+    this.weight = weight;
+    this.topSpeed = topSpeed;
+    this.towingCapacity = towingCapacity;
     // TODO: The constructor should check if the wheels array has 4 elements and create 4 new default Wheel objects if it does not
 
   // TODO: Implement the tow method from the AbleToTow interface
@@ -28,7 +56,7 @@ class Truck {
     // TODO: The method should call the printDetails method of the parent class
     // TODO: The method should log the details of the Truck
     // TODO: The details should include the VIN, make, model, year, weight, top speed, color, towing capacity, and wheels
+  }
 }
-
 // Export the Truck class as the default export
 export default Truck;
