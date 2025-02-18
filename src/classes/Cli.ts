91a58cb77be71e34a0a78b7ca4195ee29e551161
Falmanza94 <fischer.almanza@gmail.com>
2025-02-18 15:24:58 -0600

    added statements to perform the tow and wheelie actions -- should be the final commits to the repo in the cli.folder

--- src/classes/Cli.ts
+++ src/classes/Cli.ts
@@ -390,7 +390,28 @@ class Cli {
           }
         }
         // TODO: add statements to perform the tow action only if the selected vehicle is a truck. Call the findVehicleToTow method to find a vehicle to tow and pass the selected truck as an argument. After calling the findVehicleToTow method, you will need to return to avoid instantly calling the performActions method again since findVehicleToTow is asynchronous.
+        else if (answers.action === 'Tow a vehicle') {
+          for (let i = 0; i <this.vehicles.length; i++) {
+            if (this.vehicles[i].vin === this.selectedVehicleVin) {
+              if (this.vehicles[i] instanceof Truck)
+              {
+                this.findVehicleToTow(this.vehicles[i] as Truck); return
+              }else {console.log(`This vehicle cannot tow another vehicle`);
+              }
+            }
+          }
+        }
         // TODO: add statements to perform the wheelie action only if the selected vehicle is a motorbike
+        else if (answers.action === 'Perform a wheelie') {
+          for (let i = 0; i <this.vehicles.length; i++) {
+            if (this.vehicles[i].vin === this.selectedVehicleVin) {
+              if (this.vehicles[i] instanceof Motorbike) {
+                 (this.vehicles[i] as Motorbike).wheelie();
+              }else {console.log(`This vehicle cannot do a wheelie`);
+              }
+            }
+          }
+        }
         else if (answers.action === 'Select or create another vehicle') {
           // start the cli to return to the initial prompt if the user wants to select or create another vehicle
           this.startCli();
@@ -419,6 +440,7 @@ class Cli {
         },
       ])
       .then((answers) => {
+
         // check if the user wants to create a new vehicle or select an existing vehicle
         if (answers.CreateOrSelect === 'Create a new vehicle') {
           this.createVehicle();
