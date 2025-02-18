99de9aab8115dd3ea49e0ed77a01ead20057ebe8
Falmanza94 <fischer.almanza@gmail.com>
2025-02-17 20:17:19 -0600

    made changes throughout the fist 300 lines to free up the code from errors -- see previous commit for clarification

--- src/classes/Cli.ts
+++ src/classes/Cli.ts
@@ -68,13 +68,12 @@ class Cli {
         if (answers.vehicleType === 'Car') {
           // create a car
           this.createCar();
-        }
         // TODO: add statements to create a truck or motorbike if the user selects the respective vehicle type
       } else if (answers.vehicleType === 'Truck') {
         this.createTruck();
       } else if (answers.vehicleType === 'Motorbike'){
         this.createMotorbike();
-        }
+      }
     });
   }
 
@@ -253,9 +252,23 @@ class Cli {
       ])
       .then((answers) => {
         // TODO: Use the answers object to pass the required properties to the Motorbike constructor
+        const motorbike = new Motorbike(
+          Cli.generateVin(),
+          answers.color,
+          answers.make,
+          answers.model,
+          parseInt(answers.year),
+          parseInt(answers.weight),
+          parseInt(answers.topSpeed),
+          [new Wheel(parseInt(answers.frontWheelDiameter), answers.frontWheelBrand),
+           new Wheel(parseInt(answers.rearWheelDiameter), answers.rearWheelBrand)],
+       );
         // TODO: push the motorbike to the vehicles array
+        this.vehicles.push(motorbike);
         // TODO: set the selectedVehicleVin to the vin of the motorbike
+        this.selectedVehicleVin = motorbike.vin;
         // TODO: perform actions on the motorbike
+        this.performActions();
       });
   }
 
